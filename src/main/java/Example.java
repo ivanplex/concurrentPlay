5b7d5adb1c18af249b50b468562176001d5c6bb7
Ivan Chan <ivan@ivanplex.com>
2020-02-25 12:05:21 +0000

    Example class

--- src/main/java/Example.java
+++ src/main/java/Example.java
@@ -2,17 +2,24 @@ package main.java;
 
 import java.time.Instant;
 
-public class main {
+public class Example {
 
+    /**
+     * Example code on how to run the Analytics function
+     * @param args
+     */
     public static void main(String[] args){
         try {
+            // Create a list of dummy VideoPlayInfo
             VideoPlayInfo one = new VideoPlayInfo(Instant.now().minusSeconds(5), Instant.now().minusSeconds(3));
             VideoPlayInfo two = new VideoPlayInfo(Instant.now().minusSeconds(9), Instant.now().minusSeconds(3));
-
             VideoPlayInfo[] plays = {one, two};
-            Analytics analytics = new Analytics();
 
-            System.out.println("Max Watching:" + analytics.getMaximumConcurrentPlays(plays));
+            // Create new class
+            Analytics analytics = new Analytics();
+            int maxConcurrency = analytics.getMaximumConcurrentPlays(plays);
+
+            System.out.println("Max Watching: " + maxConcurrency);
         } catch (Exception e) {
             e.printStackTrace();
         }
